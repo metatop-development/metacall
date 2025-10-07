1ead823e2426976bf07bc5aa716ef5bfa3a65743
metatop-development <metatop.development@outlook.com>
2025-10-07 19:07:01 +0200

    PlatformTarget angepasst

--- metaCall.BusinessLayer/Properties/AssemblyInfo.cs
+++ metaCall.BusinessLayer/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Security.Permissions;
 // die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle("metaCallBusinessLayer")]
 [assembly: AssemblyDescription("")]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution=true)]
-[assembly: PermissionSet(SecurityAction.RequestMinimum, Name="LocalIntranet")]
+//[assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution=true)]
+//[assembly: PermissionSet(SecurityAction.RequestMinimum, Name="LocalIntranet")]
 
 // Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
 // für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
